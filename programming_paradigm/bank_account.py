017fa0bfa33c14a14180fd65ab6f927ba14b2596
Emmanuel1599 <aboagyedemo123@gmail.com>
2025-06-13 14:09:30 -1200

    Implement BankAccount OOP with command line interaction

--- programming_paradigm/bank_account.py
+++ programming_paradigm/bank_account.py
@@ -1,6 +1,18 @@
-class BankAccount:
-    def __init__(self, balance=100):
-        self.balance = balance
+# programming_paradigm/bank_account.py
 
-    def get_balance(self):
-        return self.balance
+class BankAccount:
+    def __init__(self, initial_balance=0):
+        self.__account_balance = initial_balance
+
+    def deposit(self, amount):
+        self.__account_balance += amount
+
+    def withdraw(self, amount):
+        if amount > self.__account_balance:
+            return False
+        self.__account_balance -= amount
+        return True
+
+    def display_balance(self):
+        print(f"Current Balance: ${self.__account_balance}")
+
